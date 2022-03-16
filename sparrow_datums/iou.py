052b494dc0a9c568b92f864c939b1bfbeaa20ccd
jbencook <ben@sparrow.dev>
2022-03-16 20:23:28 +0000

    pairwise iou

--- sparrow_datums/iou.py
+++ sparrow_datums/iou.py
@@ -1,40 +1,65 @@
 import numpy as np
 
 from sparrow_datums import Boxes
+from sparrow_datums.boxes import FrameBoxes
 
 
-def pairwise_iou(boxes_a: Boxes, boxes_b: Boxes) -> np.ndarray:
+def intersection(boxes_a: Boxes, boxes_b: Boxes) -> np.ndarray:
     """
-    Intersection over Union for two sets of boxes
+    Intersection areas of two sets of boxes
 
     Parameters
     ----------
     boxes_a : Boxes
-        A 2D array of boxes
+        A boxes array
     boxes_b : Boxes
-        A 2D array of boxes
+        A boxes array
 
     Returns
     -------
-    overlaps : np.ndarray
-        A (n_boxes_a, n_boxes_b) matrix of IoU scores
+    intersections : np.ndarray
+        An ndarray of intersection areas
     """
-    # TODO: put this constraint on the boxes class
-    assert boxes_a.ndim == 2 and boxes_b.ndim == 2
-    boxes_a = boxes_a.to_tlbr()
-    boxes_b = boxes_b.to_tlbr()
-    return boxes_a.array.mean()
-    # # Define the inner box
-    # x1 = max(a[0], b[0])
-    # y1 = max(a[1], b[1])
-    # x2 = min(a[2], b[2])
-    # y2 = min(a[3], b[3])
-    # # Area of a, b separately
-    # a_area = (a[2] - a[0]) * (a[3] - a[1])
-    # b_area = (b[2] - b[0]) * (b[3] - b[1])
-    # total_area = a_area + b_area
-    # # Area of inner box
-    # intersection = max(0, x2 - x1) * max(y2 - y1, 0)
-    # # Area of union
-    # union = total_area - intersection
-    # return intersection / union
+    a = boxes_a.to_tlbr().array
+    b = boxes_b.to_tlbr().array
+    # Define the inner box
+    x1 = np.maximum(a[..., 0], b[..., 0])
+    y1 = np.maximum(a[..., 1], b[..., 1])
+    x2 = np.minimum(a[..., 2], b[..., 2])
+    y2 = np.minimum(a[..., 3], b[..., 3])
+    widths = np.absolute(x2 - x1)
+    heights = np.absolute(y2 - y1)
+    return np.maximum(widths, 0) * np.maximum(heights, 0)
+
+
+def area(boxes: Boxes) -> np.ndarray:
+    """
+    Areas of boxes
+
+    Parameters
+    ----------
+    boxes : Boxes
+        A boxes array
+
+    Returns
+    -------
+    areas : np.ndarray
+        An ndarray of areas
+    """
+    x = boxes.to_tlbr().array
+    widths = np.absolute(x[..., 2] - x[..., 0])
+    heights = np.absolute(x[..., 3] - x[..., 1])
+    return widths * heights
+
+
+def pairwise_iou(boxes_a: FrameBoxes, boxes_b: FrameBoxes) -> np.ndarray:
+    """
+    Pairwise IoU for two sets of frame boxes
+    """
+    boxes_a = boxes_a.to_tlbr().view(Boxes)[:, None, :]
+    boxes_b = boxes_b.to_tlbr().view(Boxes)[None, :, :]
+    intersections = intersection(boxes_a, boxes_b)
+    areas = area(boxes_a) + area(boxes_b)
+    unions = areas - intersections
+    unions[unions == 0] = np.inf
+    return intersections / unions
--- sparrow_datums/iou_test.py
+++ sparrow_datums/iou_test.py
@@ -1,12 +1,38 @@
 import numpy as np
 
-from sparrow_datums import Boxes, BoxType
-from sparrow_datums.iou import pairwise_iou
+from sparrow_datums import Boxes, BoxType, FrameBoxes, SingleBox
+from sparrow_datums.iou import area, intersection, pairwise_iou
 
 
-def test_pairwise_iou_on_tlwh_copies():
-    x = np.concatenate([np.ones((5, 2)), np.zeros((5, 2))], -1)
-    boxes = Boxes(x, BoxType.relative_tlwh)
-    _ = pairwise_iou(boxes, boxes)
-    assert boxes.is_tlwh
-    np.testing.assert_equal(boxes.array, x)
+def test_intersection_calculation():
+    box_a = SingleBox(np.ones(4), BoxType.absolute_tlwh)
+    box_b = SingleBox(np.ones(4) * 2, BoxType.absolute_tlwh)
+    assert intersection(box_a, box_a) == 1
+    assert intersection(box_b, box_b) == 4
+    assert intersection(box_a, box_b) == 0
+
+
+def test_intersection_works_on_frame_boxes():
+    boxes = FrameBoxes(np.ones((4, 4)), BoxType.absolute_tlwh)
+    result = intersection(boxes, boxes)
+    np.testing.assert_equal(result, np.ones(4))
+
+
+def test_pairwise_intersection_works():
+    boxes = Boxes(np.ones((5, 4)), BoxType.absolute_tlwh)
+    result = intersection(boxes[:, None], boxes[None])
+    assert result.shape == (5, 5)
+    np.testing.assert_equal(result, 1)
+
+
+def test_area_calculation():
+    box = SingleBox(np.ones(4) * 3, BoxType.absolute_tlwh)
+    assert area(box) == 9
+    boxes = FrameBoxes(np.stack([np.arange(3)] * 4, -1), BoxType.absolute_tlwh)
+    np.testing.assert_array_equal(area(boxes), np.array([0, 1, 4]))
+
+
+def test_pairwise_iou_creates_pairwise_shape():
+    boxes = FrameBoxes(np.ones((5, 4)), BoxType.absolute_tlwh)
+    result = pairwise_iou(boxes, boxes)
+    assert result.shape == (5, 5)
