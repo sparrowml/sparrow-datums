f529cd5b1f963b4c616d7529f7af6c1e4d8685c2
jbencook <ben@sparrow.dev>
2022-03-18 14:53:48 +0000

    frame boxes iterator -> single box

--- sparrow_datums/boxes/frame_boxes.py
+++ sparrow_datums/boxes/frame_boxes.py
@@ -1,7 +1,10 @@
+from typing import Iterator
+
 import numpy as np
 
 from .augmented_boxes import AugmentedBoxes
 from .boxes import Boxes
+from .single_box import SingleAugmentedBox, SingleBox
 
 
 def _is_2d(x: np.ndarray) -> None:
@@ -14,8 +17,16 @@ class FrameBoxes(Boxes):
         super().validate()
         _is_2d(self)
 
+    def __iter__(self) -> Iterator[SingleBox]:
+        for box in self.view(Boxes):
+            yield box.view(SingleBox)
+
 
 class FrameAugmentedBoxes(AugmentedBoxes):
     def validate(self) -> None:
         super().validate()
         _is_2d(self)
+
+    def __iter__(self) -> Iterator[SingleAugmentedBox]:
+        for box in self.view(AugmentedBoxes):
+            yield box.view(SingleAugmentedBox)
--- sparrow_datums/boxes/frame_boxes_test.py
+++ sparrow_datums/boxes/frame_boxes_test.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 
 from .frame_boxes import FrameAugmentedBoxes, FrameBoxes
+from .single_box import SingleAugmentedBox, SingleBox
 from .types import BoxType
 
 
@@ -18,6 +19,13 @@ def test_frame_boxes_with_bad_shape_throws_value_error():
         FrameBoxes(np.ones((2, 3)))
 
 
+def test_frame_boxes_iterator_makes_single_boxes():
+    boxes = FrameBoxes(np.ones((2, 4)), BoxType.relative_tlbr)
+    for box in boxes:
+        assert isinstance(box, SingleBox)
+        assert box.type == BoxType.relative_tlbr
+
+
 def test_frame_augmented_boxes_conversion_creates_frame_augmented_boxes():
     boxes_a = FrameAugmentedBoxes(np.ones((2, 6)), type=BoxType.absolute_tlwh)
     boxes_b = boxes_a.to_tlbr()
@@ -30,3 +38,10 @@ def test_frame_augmented_boxes_with_bad_shape_throws_value_error():
         FrameAugmentedBoxes(np.ones(6))
     with pytest.raises(ValueError):
         FrameAugmentedBoxes(np.ones((2, 4)))
+
+
+def test_frame_augmented_boxes_iterator_makes_frame_augmented_boxes():
+    boxes = FrameAugmentedBoxes(np.ones((2, 6)), type=BoxType.absolute_tlwh)
+    for box in boxes:
+        assert isinstance(box, SingleAugmentedBox)
+        assert box.type == BoxType.absolute_tlwh
