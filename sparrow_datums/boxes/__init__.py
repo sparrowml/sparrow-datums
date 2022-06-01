48b7d20445779094c2daef0d9f931b5e1511ff9d
jbencook <ben@sparrow.dev>
2022-06-01 13:56:48 +0000

    expose non-max suppression

--- sparrow_datums/boxes/__init__.py
+++ sparrow_datums/boxes/__init__.py
@@ -4,5 +4,6 @@ from .boxes import Boxes
 from .frame_augmented_boxes import FrameAugmentedBoxes
 from .frame_boxes import FrameBoxes
 from .iou import area, intersection, pairwise_iou
+from .nms import non_max_suppression
 from .single_augmented_box import SingleAugmentedBox
 from .single_box import SingleBox
